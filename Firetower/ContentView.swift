9ca25cb7b5e396e2f40c4901078827a111d68767
joelshaw <shawji@me.com>
2020-04-24 18:52:52 -0400

    Init tvOS app

---
+++ Firetower/ContentView.swift
@@ -0,0 +1,21 @@
+i//
+//  ContentView.swift
+//  Firetower
+//
+//  Created by Joel Shaw on 4/24/20.
+//  Copyright © 2020 Joel Shaw. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
